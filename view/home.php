cdc6535baa6df0b4b1d63f5c5a50b05980ed2c51
yasmeen <yasmeen144@yahoo.com>
2025-02-21 06:24:20 +0200

    last eddit in home page

--- view/home.php
+++ view/home.php
@@ -1,4 +1,23 @@
+<?php
+require_once 'config/db_connection.php';
 
+if(!isset($_SESSION['username'])){
+    header("location:index.php?page=login");
+    exit;
+}
+$sql = "SELECT posts.* , users.name FROM posts JOIN users ON posts.user_id=users.id ORDER BY posts.created_at DESC";
+try{
+$query = mysqli_query($conn , $sql);
+if(!$query){
+    $_SESSION['errors'] = "connection failed" . mysqli_connect_error($conn);
+    header("location:" . $_SERVER['HTTP_REFERER']);
+    exit;
+}
+}catch(Exception $ex){
+    header("location:" . $_SERVER['HTTP_REFERER']);
+    exit;
+}
+?>
       <!DOCTYPE html>
       <html lang="en">
       <!-- Main Content-->
@@ -6,8 +25,13 @@
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
                     <!-- Post preview-->
+                     <?php while($row = mysqli_fetch_assoc($query)):
+                
+                      //var_dump($row);
+                  
+                      ?>
                     <div class="post-preview">
-                        <a href="post.php">
+                        <a href="index.php?page=post">
                             <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
                             <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
                         </a>
